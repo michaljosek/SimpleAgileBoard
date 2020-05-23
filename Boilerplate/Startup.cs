758c2f562e4120887ff9edf27f1208afef8103a9
Michał Josek <imako89@gmail.com>
2020-05-23 18:27:13 +0200

    dbcontext provider use sql server in Startup

--- Boilerplate/Startup.cs
+++ Boilerplate/Startup.cs
@@ -1,6 +1,8 @@
+using Boilerplate.EF;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +28,9 @@ namespace Boilerplate
             {
                 configuration.RootPath = "ClientApp/build";
             });
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
